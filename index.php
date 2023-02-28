4277d3e1f257f328f49622d53cd9c06b03854aae
Lucas <lucasvmorales@protonmail.com>
2023-02-28 11:36:19 -0300

    (feat): Adding PDO tab

--- index.php
+++ index.php
@@ -4,11 +4,19 @@ namespace DebugBar;
 require 'vendor/autoload.php';
 
 use DebugBar\StandardDebugBar;
+use DebugBar\DataCollector\PDO\PDOCollector;
 
 $debugbar = new StandardDebugBar();
 $debugbarRenderer = $debugbar->getJavascriptRenderer();
 
-$debugbar["messages"]->addMessage("hello world!");
+$pdo = new \PDO('mysql:host=tmdsv2.cyw3t2arq3fo.us-east-1.rds.amazonaws.com;dbname=egali_tm_dsv', 'admin', 'pbmTn9xGwiZ2i51VkuTWQ');
+$pdoCollector = new PDOCollector($pdo);
+$debugbar->addCollector($pdoCollector);
+
+$stmt = $pdo->prepare('SELECT * FROM tmbusiness');
+$stmt->execute();
+$pdoCollector->addQuery('SELECT * FROM tmbusiness', $stmt->fetchAll());
+
 ?>
 
 <!DOCTYPE html>
@@ -18,7 +26,7 @@ $debugbar["messages"]->addMessage("hello world!");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>DebugBar Example</title>
 </head>
 <body>
     <?php echo $debugbarRenderer->render() ?>
